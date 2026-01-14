5363db6b9f65f44c58b902b966ddb0a7446ceaa5
Julia Da Silva <julia6pds@gmail.com>
2026-01-14 09:21:37 -0500

    added searchbar to admin page

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -78,7 +78,7 @@ export default function Login() {
               />
             </div>
 
-            {/*
+            {/* Removal of Proper text until patch has been deployed
             <p className="py-8">
               <Link
                 to="/forgotpassword"
--- src/pages/Dashboard/Home/Home.jsx
+++ src/pages/Dashboard/Home/Home.jsx
@@ -11,9 +11,9 @@ const Home = () => {
         <h2 className={styles.sectionTitle}>Home</h2>
         <div className={styles.divider}></div>
         <h1 className={styles.greeting}>Hi, {firstName}!</h1>
-        <p className={styles.welcome}>Welcome to ElleHacks 26</p>
+        <p className={styles.welcome}>Welcome to ElleHacks 2026</p>
         <p className={styles.description}>
-          ElleHacks 26 is a free, student-run, and beginner-friendly hackathon that serves
+          ElleHacks 2026 is a free, student-run, and beginner-friendly hackathon that serves
           as a canvas to pitch bold solutions to global challenges, participate in
           engaging workshops, and connect with a diverse community of recruiters,
           industry professionals, and peers.
@@ -24,6 +24,11 @@ const Home = () => {
           hacker application. Once submitted, you’ll be able to track the status of
           your application.
         </p>
+
+        <p className={styles.description}>
+          Once applications close, we’ll review your submission. Keep an eye on your email for updates and next steps.
+        </p>
+
 
       </div>
     </div>
